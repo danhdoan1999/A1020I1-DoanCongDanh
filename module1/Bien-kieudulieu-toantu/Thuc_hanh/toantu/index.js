26365ce17e5e4aefd102aa60213a015b527112a8
Your Name <danhdoan199999@gmail.com>
2020-11-20 18:06:05 +0700

    Add baitap bien kieui du lieu

---
+++ module1/Bien-kieudulieu-toantu/Thuc_hanh/toantu/index.js
@@ -0,0 +1,4 @@
+var chieurong = parseInt(prompt("nhập vào chiều rộng "))
+var chieudai = parseInt(prompt("nhập vào chiều dài "))
+var dientich = chieudai*chieurong
+document.write("diện tích hình chữ nhật là " + dientich)
