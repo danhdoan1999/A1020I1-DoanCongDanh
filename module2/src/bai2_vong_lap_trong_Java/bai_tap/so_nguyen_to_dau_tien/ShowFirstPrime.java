57a34bfce4b4c91bf81996c76b5fb91a32222d33
Your Name <danhdoan199999@gmail.com>
2021-01-17 19:34:44 +0700

    Them phan thuc hanh mang

--- module2/src/bai2_vong_lap_trong_Java/bai_tap/so_nguyen_to_dau_tien/ShowFirstPrime.java
+++ module2/src/bai2_vong_lap_trong_Java/bai_tap/so_nguyen_to_dau_tien/ShowFirstPrime.java
@@ -8,8 +8,19 @@ public class ShowFirstPrime {
         System.out.println("Nhap vao so luong so nguyen to can in ra ");
         int number = input.nextInt();
         int count = 0;
-        int n = 2;
-        while (count <= number){
-            
+        int n = 1;
+        while (number > count){
+            int dem = 0;
+            for (int i = 1 ;i <= n ; i++){
+                if(n%i==0){
+                    dem++;
+                }
+            }
+            if(dem == 2){
+                System.out.print(n + " ");
+                count++;
+            }
+            n++;
+        }
     }
 }
